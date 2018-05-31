c57e8a175d1958f171ffb13e2e4640bcb8cd44a6
kekaadrenalin <kekapor@outlook.com>
2018-05-31 15:10:16 +0300

    update index js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import _InputTextGroup from './components/InputTextGroup';
 import _InputTextMaskGroup from './components/InputTextMaskGroup';
+import _InputEmailGroup from './components/InputEmailGroup';
+import _InputPhoneGroup from './components/InputPhoneGroup';
+import _InputNumberGroup from './components/InputNumberGroup';
 
 const InputTextGroup = _InputTextGroup;
 const InputTextMaskGroup = _InputTextMaskGroup;
+const InputEmailGroup = _InputEmailGroup;
+const InputPhoneGroup = _InputPhoneGroup;
+const InputNumberGroup = _InputNumberGroup;
 
-export {InputTextGroup, InputTextMaskGroup};
+export {InputTextGroup, InputTextMaskGroup, InputEmailGroup, InputPhoneGroup, InputNumberGroup};
