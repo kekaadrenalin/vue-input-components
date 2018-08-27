5bbc5034550d558df7e612f4b1f9c8f88d5b15fb
kekaadrenalin <kekapor@outlook.com>
2018-08-27 12:17:46 +0300

    update project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import InputTextGroup from '../components/input/InputTextGroup'
+import InputTextMaskGroup from '../components/input/InputTextMaskGroup'
+import InputEmailGroup from '../components/input/InputEmailGroup'
+import InputPhoneGroup from '../components/input/InputPhoneGroup'
+import InputNumberGroup from '../components/input/InputNumberGroup'
+
+import SelectDayGroup from '../components/select/SelectDayGroup'
+import SelectMonthGroup from '../components/select/SelectMonthGroup'
+import SelectYearGroup from '../components/select/SelectYearGroup'
+
+export { InputTextGroup, InputTextMaskGroup, InputEmailGroup, InputPhoneGroup, InputNumberGroup }
+export { SelectDayGroup, SelectMonthGroup, SelectYearGroup }
