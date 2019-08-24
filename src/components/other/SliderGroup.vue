4527d8271f1efc4523ee264f253545e3a96614af
kekaadrenalin <kekapor@outlook.com>
2019-08-24 13:16:37 +0400

    small fix + update SliderGroup

--- src/components/other/SliderGroup.vue
+++ src/components/other/SliderGroup.vue
@@ -2,9 +2,10 @@
     <div class="form-group form-group_slider row">
         <div class="col-xs-12 col-sm-2">
             <label class="control-label">{{ labelName }}</label>
+            <span :class="classSliderTooltip">{{ currentCount }}</span>
         </div>
         <div class="col-xs-12 col-sm-10">
-            <div :id="idName" ref="slider"></div>
+            <div :id="idName" :class="classSliderBlock" ref="slider"></div>
             <input type="hidden" class="hide" ref="inputElement">
         </div>
     </div>
@@ -19,46 +20,104 @@
     name: 'slider-group',
     data() {
       return {
-        count: 0
+        count: 0,
+        currentCount: 0,
       };
     },
     props: {
+      /**
+       * стартовое значение
+       */
       start: {
         type: Number,
         default: 0
       },
+
+      /**
+       * label заголовок
+       */
       labelName: {
         type: String,
         required: true
       },
+
+      /**
+       * шаг ползунка
+       */
       step: {
         type: Number,
         default: 1000
       },
+
+      /**
+       * min значение
+       */
       min: {
         type: Number,
         required: true
       },
+
+      /**
+       * max значение
+       */
       max: {
         type: Number,
         required: true
       },
+
+      /**
+       * суффикс значения
+       */
       suffix: {
         type: String,
         required: true
       },
+
+      /**
+       * атрибут id
+       */
       idName: {
         type: String,
         required: true
       },
+
+      /**
+       * класс tooltip для slider
+       */
+      classSliderTooltip: {
+        type: String,
+        default: 'slider-tooltip'
+      },
+
+      /**
+       * класс блока с slider
+       */
+      classSliderBlock: {
+        type: String,
+        default: 'slider-block'
+      },
+
+      /**
+       * префикс для vuex commit-а
+       */
       stepStore: {
         type: String,
         default: ''
       },
+
+      /**
+       * режим отладки
+       */
+      isDebug: {
+        type: Boolean,
+        default: false
+      },
     },
     watch: {
       count(value) {
-        this.$store.commit(this.stepStore + _.camelCase(this.idName), value);
+        if (!this.isDebug && this.stepStore) {
+          this.$store.commit(this.stepStore + _.camelCase(this.idName), value);
+        }
       }
     },
     mounted() {
@@ -68,7 +127,7 @@
       noUiSlider.create(element, {
         start: self.start,
         connect: 'lower',
-        tooltips: true,
+        tooltips: false,
         step: self.step,
         format: wNumb({
           decimals: 0,
@@ -84,11 +143,13 @@
       element.noUiSlider.on('end', function (values, handle, unencoded) {
         self.count = Math.round(unencoded[handle]);
       });
+
+      element.noUiSlider.on('update', function (values, handle, unencoded) {
+        self.currentCount = values[handle];
+      });
     },
     created() {
       this.count = this.start;
     },
   }
 </script>
-
-<style></style>
